96461d94e7e461b79c1eb423b4247097699d454b
nofear3377 <MaryiWilliamsw31@gmail.com>
2024-05-15 15:51:55 +0800

    Update bert.py

--- models/bert.py
+++ models/bert.py
@@ -34,13 +34,20 @@ class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
         self.bert = BertModel.from_pretrained(config.bert_path)
+        #循环遍历参数，更新bert模型参数
         for param in self.bert.parameters():
             param.requires_grad = True
+        #全连接层，将输出映射到最终的分类标签
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
 
+    #向前传播方法
     def forward(self, x):
         context = x[0]  # 输入的句子
         mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
+        #将输入的句子和掩码传递给BERT模型，得到句子的表示。这里只返回了BERT模型最后一层的输出，而不是所有层的输出。
+        #pooled是句子的[CLS]标记的输出，通常用作整个句子的表示
         _, pooled = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
+        #将BERT输出的句子表示pooled传递给全连接层，得到最终的分类结果
         out = self.fc(pooled)
+        #返回分类结果
         return out
